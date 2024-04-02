5beb4e47c937c1065390aab60566ad32568bbc84
Hiransanjeewa <hiransanjeewaa@gmail.com>
2024-04-03 00:17:13 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Student-Management-System-Client-pipeline-For-Friend/",
   plugins: [react()],
 })
