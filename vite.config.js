54b7bc2bd943b8480c07d177c767d180e5e37b8f
Hiransanjeewa <hiransanjeewaa@gmail.com>
2024-04-21 01:06:00 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Student-Management-System-Client-pipeline-For-Friend/",
+  base: "/",
   plugins: [react()],
-})
+  preview: {
+   port: 5173,
+   strictPort: true,
+  },
+  server: {
+   port: 5173,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:5173",
+  },
+ });
